b843aaa5c89c54f1fa453399d3964b87638dee37
AnmolSR98 <anmolratolcoding@gmail.com>
2023-08-19 20:34:08 -0600

    Update stratifiedsampling.sql

--- stratifiedsampling.sql
+++ stratifiedsampling.sql
@@ -3,7 +3,7 @@ BEGIN
 	RETURN (
       	SELECT SUM(Ni_array[])
 		(SELECT country, COUNT(country) AS n_i, AVG(age) AS u_i, VARIANCE(age) AS SampVar_i, 
-		ROW_NUMBER() OVER(ORDER BY country DESC) AS i, Ni_array
+		ROW_NUMBER() OVER(ORDER BY country DESC) AS i, Ni_array as Ni, N 
 		FROM Customers
 		GROUP BY country) as Data_Summary
   );
