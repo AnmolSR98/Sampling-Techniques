510a6c05bc77f64bb227720c830ac3b9948a36a3
AnmolSR98 <anmolratolcoding@gmail.com>
2023-08-19 20:32:44 -0600

    Create stratifiedsampling.sql

---
+++ stratifiedsampling.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION getStratSum(N INT, Ni_array AS INT[]) RETURNS INT
+BEGIN
+	RETURN (
+      	SELECT SUM(Ni_array[])
+		(SELECT country, COUNT(country) AS n_i, AVG(age) AS u_i, VARIANCE(age) AS SampVar_i, 
+		ROW_NUMBER() OVER(ORDER BY country DESC) AS i, Ni_array
+		FROM Customers
+		GROUP BY country) as Data_Summary
+  );
+  END
